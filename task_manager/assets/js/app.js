c44d4f24a5d4b15babe0e6270a8818799b0ed43d
nati-m <it.natnael.mekonnen@gmail.com>
2021-01-25 10:49:57 +0300

    finshed lab work for task manager

---
+++ task_manager/assets/js/app.js
@@ -0,0 +1,71 @@
+// UI vars 
+// Selecting ui items
+const taskInput = document.querySelector('#task');
+const form = document.querySelector('#task-form'); 
+const filter = document.querySelector('#filter'); 
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const reloadIcon = document.querySelector('.fa'); 
+
+// Adding event listners
+form.addEventListener("submit", addNewTask);
+filter.addEventListener("keyup", filterTasks);
+clearBtn.addEventListener("click", clearAllTasks);
+taskList.addEventListener("click", removeTask);
+reloadIcon.addEventListener('click', reloadPage);
+
+
+
+// Event handlers
+function addNewTask(e){
+    e.preventDefault();
+    
+    if (!taskInput.value){
+        // taskInput.classList.add("task-input-warning");
+        taskInput.style.borderColor = "red"
+        return;
+    }
+
+    // Create an li element when the user adds a task
+    const li = document.createElement('li');
+    // Adding a class
+    li.className = 'collection-item';
+    // Create text node and append it
+    li.appendChild(document.createTextNode(taskInput.value));
+    // Create new element for the link
+    const link = document.createElement('a');
+    // Add class and the x marker for a
+    link.className = 'delete-item secondary-content';
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+    // Append link to li
+    li.appendChild(link);
+    // Append to ul
+    taskList.appendChild(li);
+    taskInput.value = '';
+    // taskInput.classList.remove("task-input-warning");
+    taskInput.style.borderColor = "black";
+}
+
+function filterTasks(e){
+    
+}
+
+function clearAllTasks(e){
+    while (taskList.firstChild){
+        taskList.removeChild(taskList.firstChild);
+    }
+}
+
+function removeTask(e){
+    if (e.target.parentElement.classList.contains('delete-item')){
+        if (confirm('Are You Sure about that ?')){
+            e.target.parentElement.parentElement.remove();
+        }
+    }
+
+}
+
+function reloadPage() {
+    //using the reload fun on location object
+    location.reload();
+}
