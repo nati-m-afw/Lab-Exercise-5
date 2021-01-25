0cc4c13794f3ec38acc84d4b97e4e438367ea499
nati-m <it.natnael.mekonnen@gmail.com>
2021-01-25 11:50:57 +0300

    updated bom.js

--- task_manager/assets/js/bom.js
+++ task_manager/assets/js/bom.js
@@ -18,7 +18,29 @@ Good Luck !!!
 
 
 // Define UI Variables  here 
+const answers = document.querySelectorAll("span.badge");
 
+
+answers[0].innerHTML = location.href;
+answers[1].innerHTML = location.protocol;
+answers[2].innerHTML = location.host;
+answers[3].innerHTML = location.port;
+answers[4].innerHTML = location.hostname;
+
+
+answers[5].innerHTML = navigator.appName;
+answers[6].innerHTML = navigator.appVersion;
+answers[7].innerHTML = navigator.platform;
+answers[8].innerHTML = navigator.language;
+answers[9].innerHTML = navigator.cookieEnabled;
+
+
+answers[10].innerHTML = screen.height;
+answers[11].innerHTML = screen.width
+answers[12].innerHTML = screen.pixelDepth;
+
+answers[13].innerHTML = history.length;
+answers[14].innerHTML = history.state;
 
 
 
